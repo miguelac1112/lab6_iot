a6a4388f66d372aba1e7fb674806d3c6d4b32cb5
Miguel <a20190050@pucp.edu.pe>
2023-11-05 10:52:54 -0500

    xd

--- app/src/main/java/com/example/lab6/MainActivity.java
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPuzzle;
+    private Button btnMemory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPuzzle = findViewById(R.id.btnPuzzle);
+        btnMemory = findViewById(R.id.btnMemory);
+
+        btnPuzzle.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, Puzzle.class);
+            startActivity(intent);
+        });
+
+        btnMemory.setOnClickListener(view ->  {
+
+            /*Intent intent = new Intent(MainActivity.this, Puzzle.class);
+            startActivity(intent);*/
+
+        });
     }
 }
